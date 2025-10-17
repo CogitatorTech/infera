11ad793427148f6ecd05cd6517defc7c95180e90
Hassan Abedi <hassan.abedi.t@gmail.com>
2025-10-17 08:56:27 +0200

    Updated the version to `0.2.0`

--- infera/bindings/infera_extension.cpp
+++ infera/bindings/infera_extension.cpp
@@ -481,7 +481,7 @@ static void LoadInternal(ExtensionLoader &loader) {
 
 void InferaExtension::Load(ExtensionLoader &loader) { LoadInternal(loader); }
 std::string InferaExtension::Name() { return "infera"; }
-std::string InferaExtension::Version() const { return "v0.1.0"; }
+std::string InferaExtension::Version() const { return "v0.2.0"; }
 
 } // namespace duckdb
 
